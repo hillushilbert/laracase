c121687f201eb4bd040e8a881f7fa46ffaa076fc
Hillus Hilbert <hillus.hilbert@codding.com.br>
2022-03-24 11:56:10 -0300

    fix: adicionando cdn jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,10 @@
     <title>Laracase</title>
 
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="{{ asset('js/jquery-3.3.1.min.js') }}" ></script>
+    <script
+			  src="https://code.jquery.com/jquery-3.6.0.min.js"
+			  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+			  crossorigin="anonymous"></script>
 
     @yield('addHead')
 	
