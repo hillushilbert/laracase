9d89673df7bb6e15e57f3c464246ca155eacf8cf
Hillus Hilbert <hillus.hilbert@codding.com.br>
2022-03-24 11:53:02 -0300

    fix: removendo dependencia de rotas de login do layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Sin Solution</title>
+    <title>Laracase</title>
 
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="{{ asset('js/jquery-3.3.1.min.js') }}" ></script>
@@ -24,8 +24,7 @@
     <link rel="stylesheet" href="{{ asset('font/fontawesome-free-5.7.2-web/css/all.css') }}">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-
+    
     <style>
         html, body {
             width: 100%;
@@ -60,7 +59,7 @@
 <body>
     <div id="app">
 
-        @include('layouts.menu')
+        @include('laracase::layouts.menu')
 
         <main class="py-4">
             @yield('content')
